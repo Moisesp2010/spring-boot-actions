28691523594db5561aee0d080a8132888019329f
Moisesp2010 <moisespe2010@gmail.com>
2022-10-10 12:19:47 -0300

    importante

---
+++ src/main/java/com/springactions/springbootactions/SpringBootActionsApplication.java
@@ -0,0 +1,13 @@
+package com.springactions.springbootactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springactions/springbootactions/SpringBootActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springactions.springbootactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
